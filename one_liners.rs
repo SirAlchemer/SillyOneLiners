caddb3fa16b830ff7f82261e4aef671c675311f3
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2025-07-14 22:48:11 -0700

    Update one_liners.rs

--- one_liners.rs
+++ one_liners.rs
@@ -9,8 +9,3 @@ fn longest(a1: &str, a2: &str) -> String {
             .sorted() // 6
             .join(""); // 7
 }
-
-/// Checks to see if a valid triangle can be made from 3 numbers of type i64
-fn is_triangle(a: i64, b: i64, c: i64) -> bool {
-    [a, b, c].iter().all(|&x| x > 0) && a + b > c && a + c > b && b + c > a
-}
